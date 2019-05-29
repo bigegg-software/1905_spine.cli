37a49b80e80d29c0c4215c2583f27b755a1ee865
jones0036 <jones0036@gmail.com>
2019-05-29 08:17:38 +0000

    update

--- src/cloudProvider/aliyun.js
+++ src/cloudProvider/aliyun.js
@@ -29,7 +29,7 @@ async function newapp(name) {
         throw "terraform init failed"
     }
 
-    res = spawnSync('terraform' , ['apply'], {
+    res = spawnSync('terraform' , ['apply', '-auto-approve', '-var', `vpc_name=${name}-dev`], {
         shell: true ,
         stdio: 'inherit',
         cwd: tmpDir
@@ -49,88 +49,33 @@ async function newapp(name) {
         console.error(res.error);
         throw "terraform output failed"
     }
-    let netConf = JSON.parse(res.stdout.toString())
 
-    //res = spawnSync('terraform' , ['output', '-json'], {
-    //aliyun eci CreateContainerGroup \
-    //--RegionId "$region_id" \
-    //--SecurityGroupId "$securitygroup_id" \
-    //--VSwitchId "$vswitch_id" \
-    //--ContainerGroupName example-eci-parseserver \
-    //--ZoneId "$zone_id" \
-    //--EipInstanceId "$eip_id" \
-    //--Container.1.Image mongo:4.0.5 \
-    //--Container.1.Name mongo \
-    //--Container.1.Cpu 1 \
-    //--Container.1.Memory 1 \
-    //--Container.1.Port.1.Protocol TCP \
-    //--Container.1.Port.1.Port 27017 \
-    //--Container.1.EnvironmentVar.1.Key PATH \
-    //--Container.1.EnvironmentVar.1.Value /usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-    //--Container.1.EnvironmentVar.2.Key GOSU_VERSION \
-    //--Container.1.EnvironmentVar.2.Value 1.10 \
-    //--Container.1.EnvironmentVar.3.Key JSYAML_VERSION \
-    //--Container.1.EnvironmentVar.3.Value 3.10.0 \
-    //--Container.1.EnvironmentVar.4.Key GPG_KEYS \
-    //--Container.1.EnvironmentVar.4.Value 9DA31620334BD75D9DCB49F368818C72E52529D4 \
-    //--Container.1.EnvironmentVar.5.Key MONGO_PACKAGE \
-    //--Container.1.EnvironmentVar.5.Value mongodb-org \
-    //--Container.1.EnvironmentVar.6.Key MONGO_REPO \
-    //--Container.1.EnvironmentVar.6.Value repo.mongodb.org \
-    //--Container.1.EnvironmentVar.7.Key MONGO_MAJOR \
-    //--Container.1.EnvironmentVar.7.Value 4.0 \
-    //--Container.1.EnvironmentVar.8.Key MONGO_VERSION \
-    //--Container.1.EnvironmentVar.8.Value 4.0.5 \
-    //--Container.2.Image parseplatform/parse-server \
-    //--Container.2.Name parse-server \
-    //--Container.2.Cpu 1 \
-    //--Container.2.Memory 1 \
-    //--Container.2.Port.1.Protocol TCP \
-    //--Container.2.Port.1.Port 1337 \
-    //--Container.2.EnvironmentVar.1.Key PARSE_SERVER_APPLICATION_ID \
-    //--Container.2.EnvironmentVar.1.Value 123456 \
-    //--Container.2.EnvironmentVar.2.Key PARSE_SERVER_APP_NAME \
-    //--Container.2.EnvironmentVar.2.Value example-eci \
-    //--Container.2.EnvironmentVar.3.Key PARSE_SERVER_MASTER_KEY \
-    //--Container.2.EnvironmentVar.3.Value 123456 \
-    //--Container.2.EnvironmentVar.4.Key PARSE_SERVER_READ_ONLY_MASTER_KEY \
-    //--Container.2.EnvironmentVar.4.Value 1234567 \
-    //--Container.2.EnvironmentVar.5.Key PARSE_SERVER_CLIENT_KEY \
-    //--Container.2.EnvironmentVar.5.Value 123456 \
-    //--Container.2.EnvironmentVar.6.Key PARSE_SERVER_JAVASCRIPT_KEY \
-    //--Container.2.EnvironmentVar.6.Value 123456 \
-    //--Container.2.EnvironmentVar.7.Key PARSE_SERVER_WEBHOOK_KEY \
-    //--Container.2.EnvironmentVar.7.Value 123456 \
-    //--Container.2.EnvironmentVar.8.Key PARSE_SERVER_DATABASE_URI \
-    //--Container.2.EnvironmentVar.8.Value mongodb://example-eci-parseserver:27017/parse \
-    //--Container.2.EnvironmentVar.9.Key PARSE_SERVER_MOUNT_PATH \
-    //--Container.2.EnvironmentVar.9.Value / \
-    //--Container.3.Image parseplatform/parse-dashboard \
-    //--Container.3.Name parse-dashbord \
-    //--Container.3.Cpu 1 \
-    //--Container.3.Memory 1 \
-    //--Container.3.Port.1.Protocol TCP \
-    //--Container.3.Port.1.Port 4040 \
-    //--Container.3.EnvironmentVar.1.Key PORT \
-    //--Container.3.EnvironmentVar.1.Value 4040 \
-    //--Container.3.EnvironmentVar.2.Key MOUNT_PATH \
-    //--Container.3.EnvironmentVar.2.Value / \
-    //--Container.3.EnvironmentVar.3.Key PARSE_DASHBOARD_SERVER_URL \
-    //--Container.3.EnvironmentVar.3.Value "http://$eip_ip:1337" \
-    //--Container.3.EnvironmentVar.4.Key PARSE_DASHBOARD_MASTER_KEY \
-    //--Container.3.EnvironmentVar.4.Value 123456 \
-    //--Container.3.EnvironmentVar.5.Key PARSE_DASHBOARD_APP_ID \
-    //--Container.3.EnvironmentVar.5.Value 123456 \
-    //--Container.3.EnvironmentVar.6.Key PARSE_DASHBOARD_APP_NAME \
-    //--Container.3.EnvironmentVar.6.Value example-eci \
-    //--Container.3.EnvironmentVar.7.Key PARSE_DASHBOARD_USER_ID \
-    //--Container.3.EnvironmentVar.7.Value admin \
-    //--Container.3.EnvironmentVar.8.Key PARSE_DASHBOARD_USER_PASSWORD \
-    //--Container.3.EnvironmentVar.8.Value Admin1234 \
-    //--Container.3.EnvironmentVar.9.Key PARSE_DASHBOARD_ALLOW_INSECURE_HTTP \
-    //--Container.3.EnvironmentVar.9.Value true
+    let netConf = {};
 
-    console.log(tmpDir, res.status, res.error);
+    let rawConf = JSON.parse(res.stdout.toString());
+    
+    for (let key in rawConf) {
+        if (rawConf[key] && rawConf[key].value !== undefined) {
+            netConf[key] = rawConf[key].value
+        }
+    }
+
+    let aliyunCliCreateEciArgs = ['eci', 'CreateContainerGroup']
+    res = spawnSync('aliyun' , aliyunCliCreateEciArgs, {
+        shell: true ,
+        stdio: 'inherit',
+        cwd: tmpDir
+    });
+    aliyunCliCreateEciArgs.push('--RegionId', netConf.region_id)
+    aliyunCliCreateEciArgs.push('--SecurityGroupId', netConf.securitygroup_id)
+    aliyunCliCreateEciArgs.push('--VSwitchId', netConf.vswitch_id)
+    aliyunCliCreateEciArgs.push('--ZoneId', netConf.zone_id) 
+    aliyunCliCreateEciArgs.push('--EipInstanceId', netConf.eip_id)
+    aliyunCliCreateEciArgs.push('--ContainerGroupName', `${name}-dev`)
+
+    aliyunCliCreateEciArgs.push('--Container.1.Image', 'mongo:4.0.5')
+
+console.log(tmpDir, res.status, res.error);
 }
 
 module.exports = {
